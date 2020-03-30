7cddb75cef5a423d1d4c9b816735f2bbe3a4491a
Amazing_DM <amazingdmdd@gmail.com>
2020-03-31 00:50:36 +0800

    Revert "Change STUN Server"
    This reverts commit c39b758fe6125b1bab2b2fb66502896ab9eac581.

--- Netch/Models/Setting.cs
+++ Netch/Models/Setting.cs
@@ -147,12 +147,12 @@ namespace Netch.Models
         /// <summary>
         ///     STUN测试服务器
         /// </summary>
-        public string STUN_Server = "stun.l.google.com";
+        public string STUN_Server = "stun.stunprotocol.org";
 
         /// <summary>
         ///     STUN测试服务器
         /// </summary>
-        public int STUN_Server_Port = 19302;
+        public int STUN_Server_Port = 3478;
 
         /// <summary>
         ///     是否切换为2号核心
