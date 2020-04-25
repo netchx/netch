f379aa4e65cb34dca1dbf4eee7e64a6a70247375
Bruce Wayne <admin@bige0.com>
2020-04-25 12:27:07 +0800

    Refine TCPing

--- Netch/Utils/Utils.cs
+++ Netch/Utils/Utils.cs
@@ -1,5 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Netch.Utils
 {
@@ -39,6 +44,25 @@ namespace Netch.Utils
             }
 
             return false;
+        }
+
+        public static async Task<int> TCPingAsync(IPAddress ip, int port, int timeout = 1000, CancellationToken ct = default)
+        {
+            using var client = new TcpClient(ip.AddressFamily);
+            var task = client.ConnectAsync(ip, port);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var resTask = await Task.WhenAny(Task.Delay(timeout, ct), task);
+
+            stopwatch.Stop();
+            if (resTask == task && client.Connected)
+            {
+                var t = Convert.ToInt32(stopwatch.Elapsed.TotalMilliseconds);
+                return t;
+            }
+            return timeout;
         }
     }
 }
