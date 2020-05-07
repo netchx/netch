0b46f8d8affaeddd4a541929ea2f85437fe36e16
Connection Refused <cutecr@pm.me>
2020-05-07 20:52:50 +0800

    Lowercase SHA256

--- GetSHA256.ps1
+++ GetSHA256.ps1
@@ -1,5 +1,5 @@
 param([string]$file)
 $hash = [Security.Cryptography.HashAlgorithm]::Create( "SHA256" )
 $stream = ([IO.StreamReader]$file).BaseStream
--join ($hash.ComputeHash($stream) | ForEach { "{0:X2}" -f $_ })
+-join ($hash.ComputeHash($stream) | ForEach { "{0:x2}" -f $_ })
 $stream.Close()
