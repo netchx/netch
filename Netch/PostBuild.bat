7310126a39c777c7df5a4d43081c24c569952e96
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2020-08-29 15:37:19 +0800

    PostBuild 命令移至脚本,仅 Release 合并 dll

---
+++ Netch/PostBuild.bat
@@ -0,0 +1,33 @@
+if %Configuration%==Release (
+:: Merge dlls
+%ILMergeConsolePath% %TargetDir%Netch.exe ^
+/out:%TargetDir%NetchMerged.exe ^
+%TargetDir%Dia2Lib.dll ^
+%TargetDir%Interop.NetFwTypeLib.dll ^
+%TargetDir%Interop.TaskScheduler.dll ^
+%TargetDir%MaxMind.Db.dll ^
+%TargetDir%MaxMind.GeoIP2.dll ^
+%TargetDir%Microsoft.Diagnostics.FastSerialization.dll ^
+%TargetDir%Microsoft.Diagnostics.Tracing.TraceEvent.dll ^
+%TargetDir%NetchLib.dll ^
+%TargetDir%Newtonsoft.Json.dll ^
+%TargetDir%OSExtensions.dll ^
+%TargetDir%System.Net.IPNetwork.dll ^
+%TargetDir%System.Runtime.CompilerServices.Unsafe.dll ^
+%TargetDir%TraceReloggerLib.dll
+
+DEL /f %TargetDir%*.dll >NUL 2>&1
+MOVE /Y %TargetDir%NetchMerged.exe %TargetDir%Netch.exe >NUL
+)
+
+RD /S /Q %TargetDir%bin >NUL 2>&1
+RD /S /Q %TargetDir%i18n >NUL 2>&1
+RD /S /Q %TargetDir%mode >NUL 2>&1
+
+XCOPY /s /Y %SolutionDir%binaries %TargetDir%bin\ >NUL
+XCOPY /s /Y %SolutionDir%translations\i18n %TargetDir%i18n\ >NUL
+XCOPY /s /Y %SolutionDir%modes\mode %TargetDir%mode\ >NUL
+
+DEL /f %TargetDir%*.config >NUL 2>&1
+DEL /f %TargetDir%*.pdb >NUL 2>&1
+RD /s /Q %TargetDir%x86 >NUL 2>&1
