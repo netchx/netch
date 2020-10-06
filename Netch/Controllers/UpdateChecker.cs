5490364217f97780fba4cb6710896de2390a60bc
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2020-10-06 19:18:19 +0800

    bump version to 1.6.0

--- Netch/Controllers/UpdateChecker.cs
+++ Netch/Controllers/UpdateChecker.cs
@@ -16,7 +16,7 @@ namespace Netch.Controllers
         public const string Copyright = @"Copyright © 2019 - 2020";
 
         public const string AssemblyVersion = @"1.6.0";
-        private const string Suffix = @"Beta1";
+        private const string Suffix = @"";
 
         public static readonly string Version = $"{AssemblyVersion}{(string.IsNullOrEmpty(Suffix) ? "" : $"-{Suffix}")}";
 
