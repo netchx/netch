f26d09bf9f6617a05bf49d8b4d69d0ca8b7fb87e
Bruce Wayne <admin@bige0.com>
2020-10-10 20:08:33 +0800

    Update GetSHA256.ps1

--- GetSHA256.ps1
+++ GetSHA256.ps1
@@ -1,5 +1,6 @@
 param([string]$file)
 $hash = [Security.Cryptography.HashAlgorithm]::Create( "SHA256" )
-$stream = ([IO.StreamReader]$file).BaseStream
--join ($hash.ComputeHash($stream) | ForEach { "{0:x2}" -f $_ })
+$path = (Resolve-Path -Path $file).Path
+$stream = ([IO.StreamReader]$path).BaseStream
+-join ($hash.ComputeHash($stream) | ForEach-Object { "{0:x2}" -f $_ })
 $stream.Close()
