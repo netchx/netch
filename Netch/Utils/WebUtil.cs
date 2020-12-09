59b4ba578b0e6a9dc1679fb1cdfb723773ca3d4b
AmazingDM <amazingdmdd@gmail.com>
2020-12-09 12:00:51 +0800

    Update UA

--- Netch/Utils/WebUtil.cs
+++ Netch/Utils/WebUtil.cs
@@ -14,7 +14,7 @@ namespace Netch.Utils
         }
 
         public const string DefaultUserAgent =
-            @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36";
+            @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.55";
 
         private static int DefaultGetTimeout => Global.Settings.RequestTimeout;
 
