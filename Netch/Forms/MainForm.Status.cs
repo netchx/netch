a544bd1a2632e792f5eb2f9c9607b4ca830bcfa4
AmazingDM <amazingdmdd@gmail.com>
2021-01-05 17:37:14 +0800

    ♿️

--- Netch/Forms/MainForm.Status.cs
+++ Netch/Forms/MainForm.Status.cs
@@ -80,7 +80,7 @@ namespace Netch.Forms
                         }
 
                         //Socks5无身份验证且为网页代理模式时无法统计流量不显示流量状态栏，Socks5有身份验证时将统计V2ray的流量
-                        if (s5BwFlag || Models.ModeExtension.TestNatRequired(MainController.Mode))
+                        if (s5BwFlag || MainController.Mode.ProcessOrTuntap())
                             UsedBandwidthLabel.Visible /*= UploadSpeedLabel.Visible*/ = DownloadSpeedLabel.Visible = Global.Flags.IsWindows10Upper;
                         break;
                     case State.Stopping:
