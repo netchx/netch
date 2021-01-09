325ad430fe607303bcfee91085a73f418df4c7d0
AmazingDM <amazingdmdd@gmail.com>
2021-01-09 02:33:00 +0000

    Bump version to 1.7.1

--- Netch/Controllers/UpdateChecker.cs
+++ Netch/Controllers/UpdateChecker.cs
@@ -15,7 +15,7 @@ namespace Netch.Controllers
         public const string Name = @"Netch";
         public const string Copyright = @"Copyright © 2019 - 2020";
 
-        public const string AssemblyVersion = @"1.7.0";
+        public const string AssemblyVersion = @"1.7.1";
         private const string Suffix = @"";
 
         public static readonly string Version = $"{AssemblyVersion}{(string.IsNullOrEmpty(Suffix) ? "" : $"-{Suffix}")}";
