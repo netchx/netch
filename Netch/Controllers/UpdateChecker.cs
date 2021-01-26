8e83359e391f4b2f74d1ab8476293ddd414317c2
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2021-01-26 21:57:24 +0800

    Bump version to 1.7.3

--- Netch/Controllers/UpdateChecker.cs
+++ Netch/Controllers/UpdateChecker.cs
@@ -19,7 +19,7 @@ namespace Netch.Controllers
         public const string Name = @"Netch";
         public const string Copyright = @"Copyright © 2019 - 2020";
 
-        public const string AssemblyVersion = @"1.7.2";
+        public const string AssemblyVersion = @"1.7.3";
         private const string Suffix = @"";
 
         public static readonly string Version = $"{AssemblyVersion}{(string.IsNullOrEmpty(Suffix) ? "" : $"-{Suffix}")}";
