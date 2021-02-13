18f71d3862cf4d965ad02ed76f7830c1ccfba575
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2021-02-13 12:38:36 +0800

    Update PostBuild.bat

--- Netch/PostBuild.bat
+++ Netch/PostBuild.bat
@@ -1,13 +1,15 @@
 if %Configuration%==Release (
 :: Merge dlls
-%ILMergeConsolePath% /wildcards /out:%TargetDir%NetchMerged.exe ^
+%ILMergeConsolePath% /wildcards /out:%TargetDir%Netch.exe ^
 /lib:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" ^
 /targetplatform:v4,"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.8" ^
 %TargetDir%Netch.exe ^
 %TargetDir%*.dll
 
 DEL /f %TargetDir%*.dll >NUL 2>&1
-MOVE /Y %TargetDir%NetchMerged.exe %TargetDir%Netch.exe >NUL
+
+DEL /f %TargetDir%*.config >NUL 2>&1
+DEL /f %TargetDir%*.pdb >NUL 2>&1
 )
 
 RD /S /Q %TargetDir%bin >NUL 2>&1
@@ -18,8 +20,6 @@ XCOPY /s /Y %SolutionDir%binaries %TargetDir%bin\ >NUL
 XCOPY /s /Y %SolutionDir%translations\i18n %TargetDir%i18n\ >NUL
 XCOPY /s /Y %SolutionDir%modes\mode %TargetDir%mode\ >NUL
 
-DEL /f %TargetDir%*.config >NUL 2>&1
-DEL /f %TargetDir%*.pdb >NUL 2>&1
 RD /s /Q %TargetDir%x86 >NUL 2>&1
 
 exit 0
