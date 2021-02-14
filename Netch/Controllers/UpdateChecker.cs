400b69d70b3dce71949158303220cd5e25e5c554
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2021-02-14 22:18:49 +0800

    Fix Typo

--- Netch/Controllers/UpdateChecker.cs
+++ Netch/Controllers/UpdateChecker.cs
@@ -19,7 +19,7 @@ namespace Netch.Controllers
         public const string Name = @"Netch";
         public const string Copyright = @"Copyright © 2019 - 2021";
 
-        public const string AssemblyVersion = @"1.7.3";
+        public const string AssemblyVersion = @"1.7.4";
         private const string Suffix = @"";
 
         public static readonly string Version = $"{AssemblyVersion}{(string.IsNullOrEmpty(Suffix) ? "" : $"-{Suffix}")}";
