4ab844e31c3a902a5ce4067fe1044d49ca84d25a
Connection Refused <cutecr@pm.me>
2021-03-26 18:25:08 +0800

    Update NativeMethods

--- Netch/NativeMethods.cs
+++ Netch/NativeMethods.cs
@@ -5,8 +5,20 @@ namespace Netch
     public static class NativeMethods
     {
         /// <summary>
+        ///     分配 IP 地址
+        /// </summary>
+        /// <param name="inet">AF_INET / AF_INET6</param>
+        /// <param name="address">目标地址</param>
+        /// <param name="cidr">CIDR</param>
+        /// <param name="index">适配器索引</param>
+        /// <returns>是否成功</returns>
+        [DllImport("RouteHelper.bin", CallingConvention = CallingConvention.Cdecl, EntryPoint = "CreateUnicastIP")]
+        public static extern bool CreateUnicastIP(int inet, string address, int cidr, int index);
+
+        /// <summary>
         ///     创建路由规则
         /// </summary>
+        /// <param name="inet">AF_INET / AF_INET6</param>
         /// <param name="address">目标地址</param>
         /// <param name="cidr">CIDR</param>
         /// <param name="gateway">网关地址</param>
@@ -14,11 +26,12 @@ namespace Netch
         /// <param name="metric">跃点数</param>
         /// <returns>是否成功</returns>
         [DllImport("RouteHelper.bin", CallingConvention = CallingConvention.Cdecl, EntryPoint = "CreateRoute")]
-        public static extern bool CreateRoute(string address, int cidr, string gateway, int index, int metric = 0);
+        public static extern bool CreateRoute(int inet, string address, int cidr, string gateway, int index, int metric = 0);
 
         /// <summary>
         ///     删除路由规则
         /// </summary>
+        /// <param name="inet">AF_INET / AF_INET6</param>
         /// <param name="address">目标地址</param>
         /// <param name="cidr">掩码地址</param>
         /// <param name="gateway">网关地址</param>
@@ -26,7 +39,7 @@ namespace Netch
         /// <param name="metric">跃点数</param>
         /// <returns>是否成功</returns>
         [DllImport("RouteHelper.bin", CallingConvention = CallingConvention.Cdecl, EntryPoint = "DeleteRoute")]
-        public static extern bool DeleteRoute(string address, int cidr, string gateway, int index, int metric = 0);
+        public static extern bool DeleteRoute(int inet, string address, int cidr, string gateway, int index, int metric = 0);
 
         [DllImport("dnsapi", EntryPoint = "DnsFlushResolverCache")]
         public static extern uint FlushDNSResolverCache();
