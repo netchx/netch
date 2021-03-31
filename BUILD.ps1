7ab89b67c573717257bdf545786540c8f563d26b
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2021-03-31 18:45:21 +0800

    Update BUILD.ps1

--- BUILD.ps1
+++ BUILD.ps1
@@ -1,8 +1,38 @@
-Write-Host 'Building'
+param (
+	[Alias('c')]
+	[Parameter()]
+	[ValidateSet('Debug', 'Release')]
+	[string]
+	$Configuration = 'Release',
 
-dotnet build `
-	-c "Release" `
+	[Alias('o')]
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]
+	$OutputPath = 'Netch\bin\Publish\',
+
+	[Parameter()]
+	[bool]
+	$SelfContained = $False,
+
+	[Parameter()]
+	[bool]
+	$PublishReadyToRun = $False
+)
+
+$PublishSingleFile = $True
+
+Write-Host "Building $Configuration to $OutputPath"
+
+dotnet publish `
+	-c $Configuration `
+	-r "win-x64" `
 	-p:Platform="x64" `
+	-p:PublishSingleFile=$PublishSingleFile `
+	-p:SelfContained=$SelfContained `
+	-p:PublishTrimmed=$SelfContained `
+	-p:PublishReadyToRun=$PublishReadyToRun `
+	-o $OutputPath `
 	Netch\Netch.csproj
 
 if ($LASTEXITCODE) { exit $LASTEXITCODE } 
