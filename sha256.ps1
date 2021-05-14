ebe2978724e7bd0120f57432736cb3b233733ccc
Bruce Wayne <admin@bige0.com>
2021-05-14 10:16:11 +0800

    Update sha256.ps1

--- sha256.ps1
+++ sha256.ps1
@@ -1,6 +1,2 @@
 param([string]$file)
-$hash = [Security.Cryptography.HashAlgorithm]::Create( "SHA256" )
-$path = (Resolve-Path -Path $file).Path
-$stream = ([IO.StreamReader]$path).BaseStream
--join ($hash.ComputeHash($stream) | ForEach-Object { "{0:x2}" -f $_ })
-$stream.Close()
+(Get-FileHash $file -Algorithm SHA256).Hash.ToLower()
