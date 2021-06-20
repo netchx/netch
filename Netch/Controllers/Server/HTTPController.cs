dac78cce21aa2bd94410f35e4eafd0aa6c7af6bc
Netch <netch-org@pm.me>
2021-06-21 01:49:48 +0800

    Add HTTP Controller

---
+++ Netch/Controllers/Server/HTTPController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netch.Controllers.Server
+{
+    public class HTTPController : Interface.IController
+    {
+        public bool Create(Models.Server.Server s, Models.Mode.Mode m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
