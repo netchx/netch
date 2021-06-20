31b2ee3222d9a4329a6370073b9d54bfc087452b
Netch <netch-org@pm.me>
2021-06-21 01:49:14 +0800

    Add WinDivertController

---
+++ Netch/Controllers/Mode/WinDivertController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netch.Controllers.Mode
+{
+    public class WinDivertController : Interface.IController
+    {
+        public bool Create(Models.Server.Server s, Models.Mode.Mode m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
