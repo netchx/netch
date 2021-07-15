88f3a4940b22108871fd917a4e21d17793962036
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2021-07-15 14:39:52 +0800

    Add UnitTest

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
