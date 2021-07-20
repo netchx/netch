2bc7db4b9a5374c2a84f2669489a57f7c207e16d
Netch <netch-org@pm.me>
2021-07-20 08:48:35 +0800

    Update geoip and geosite

--- scripts/deps/geoip.ps1
+++ scripts/deps/geoip.ps1
@@ -1,6 +1,6 @@
 param([string]$OutputPath)
 $name="geoip.dat"
-$address="https://github.com/v2fly/geoip/releases/download/202107080024/geoip.dat"
+$address="https://github.com/v2fly/geoip/releases/download/202107150023/geoip.dat"
 
 ..\scripts\download.ps1 $name $address
 if (-Not $?) { exit $lastExitCode }
--- scripts/deps/geosite.ps1
+++ scripts/deps/geosite.ps1
@@ -1,6 +1,6 @@
 param([string]$OutputPath)
 $name="geosite.dat"
-$address="https://github.com/v2fly/domain-list-community/releases/download/20210709152606/dlc.dat"
+$address="https://github.com/v2fly/domain-list-community/releases/download/20210718155822/dlc.dat"
 
 ..\scripts\download.ps1 $name $address
 if (-Not $?) { exit $lastExitCode }
