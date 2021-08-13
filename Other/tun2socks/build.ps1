d3033511b630859e5214f863c4a6421525cc9ca2
Netch <netch-org@pm.me>
2021-08-14 04:51:34 +0800

    Update build.ps1

--- Other/tun2socks/build.ps1
+++ Other/tun2socks/build.ps1
@@ -1,11 +1,11 @@
 Push-Location (Split-Path $MyInvocation.MyCommand.Path -Parent)
 
-$Env:CGO_ENABLED="0"
-$Env:GOROOT_FINAL="/usr"
+$Env:CGO_ENABLED='0'
+$Env:GOROOT_FINAL='/usr'
 
-$Env:GOOS="windows"
-$Env:GOARCH="amd64"
-go build -a -trimpath -asmflags "-s -w" -ldflags "-s -w" -o "..\release\tun2socks.exe"
+$Env:GOOS='windows'
+$Env:GOARCH='amd64'
+go build -a -buildmode=c-shared -trimpath -asmflags '-s -w' -ldflags '-s -w' -o '..\release\tun2socks.bin'
 
 Pop-Location
 exit $lastExitCode
