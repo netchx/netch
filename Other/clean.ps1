6a94dc625bc4daebb7ed84ce4547a54637fc523f
Netch <netch-org@pm.me>
2021-09-12 23:38:21 +0800

    Update clean.ps1

--- Other/clean.ps1
+++ Other/clean.ps1
@@ -1,7 +1,15 @@
 Push-Location (Split-Path $MyInvocation.MyCommand.Path -Parent)
 
-if (Test-Path 'release') {
+if ( Test-Path 'release' ) {
     rm -Recurse -Force 'release'
+}
+
+Get-ChildItem -Path '.' -Directory | ForEach-Object {
+    $name=$_.Name
+
+    if ( Test-Path "$name\src" ) {
+        rm -Recurse -Force "$name\src"
+    }
 }
 
 Pop-Location
