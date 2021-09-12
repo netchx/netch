eb9f8940204e053c2a9d139a454e36e2faa74ee5
Netch <netch-org@pm.me>
2021-09-12 23:32:43 +0800

    Update tun2socks

--- Other/tun2socks/build.ps1
+++ Other/tun2socks/build.ps1
@@ -5,7 +5,7 @@ $Env:GOROOT_FINAL='/usr'
 
 $Env:GOOS='windows'
 $Env:GOARCH='amd64'
-go build -a -buildmode=c-shared -trimpath -asmflags '-s -w' -ldflags '-s -w' -o '..\release\tun2socks.bin'
+go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o '..\release\tun2socks.exe'
 
 Pop-Location
 exit $lastExitCode
