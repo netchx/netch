b1d3498107006bd8c48a7638621b2da4e54e7941
Connection Refused <cutecr@pm.me>
2021-09-23 14:40:35 +0800

    [GitHub CI] Build shadowsocks-rust

---
+++ Other/shadowsocks-rust/build.ps1
@@ -0,0 +1,24 @@
+Push-Location (Split-Path $MyInvocation.MyCommand.Path -Parent)
+
+git clone https://github.com/shadowsocks/shadowsocks-rust -b 'v1.11.2' src
+if ( -Not $? ) {
+    Pop-Location
+    exit $lastExitCode
+}
+Push-Location src
+
+cargo build --release
+if ( -Not $? ) {
+    Pop-Location
+
+    rm -Recurse -Force src
+    exit $lastExitCode
+}
+
+cp -Force '.target\release\sslocal.exe' '..\release\ss-local.exe'
+
+Pop-Location
+rm -Recurse -Force src
+
+Pop-Location
+exit $lastExitCode
