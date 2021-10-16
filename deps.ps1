c57d5c8696cefaecf3f75043e2e5b929563c7cea
Connection Refused <cutecr@pm.me>
2021-10-16 09:31:56 +0800

    [Scripts] Update deps.ps1

--- deps.ps1
+++ deps.ps1
@@ -12,17 +12,6 @@ New-Item -ItemType Directory -Name 'bin'  | Out-Null
 New-Item -ItemType Directory -Name 'mode' | Out-Null
 New-Item -ItemType Directory -Name 'i18n' | Out-Null
 
-# Get-ChildItem -Path '..\scripts\deps\main' -File | ForEach-Object {
-#     $name=$_.Name
-
-#     Write-Host "Executing $name"
-#     & "..\scripts\deps\base\$name"
-#     if ( -Not $? ) {
-#         Pop-Location
-#         exit $lastExitCode
-#     }
-# }
-
 Get-ChildItem -Path '..\scripts\deps' -File | ForEach-Object {
     $name=$_.Name
 
