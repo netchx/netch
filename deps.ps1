abf7c8e9949834dd14e783f5f38558b8b2083b83
Connection Refused <cutecr@pm.me>
2021-10-16 13:08:13 +0800

    [Scripts] Update deps.ps1

--- deps.ps1
+++ deps.ps1
@@ -3,7 +3,7 @@ param (
     $OutputPath = 'release'
 )
 
-if ( -Not (Test-Path -Path "$OutputPath") ) {
+if ( -Not ( Test-Path -Path "$OutputPath" ) ) {
     New-Item -ItemType Directory -Name "$OutputPath" | Out-Null
 }
 
@@ -11,17 +11,5 @@ Push-Location "$OutputPath"
 New-Item -ItemType Directory -Name 'bin'  | Out-Null
 New-Item -ItemType Directory -Name 'mode' | Out-Null
 New-Item -ItemType Directory -Name 'i18n' | Out-Null
-
-Get-ChildItem -Path '..\scripts\deps' -File | ForEach-Object {
-    $name=$_.Name
-
-    Write-Host "Executing $name"
-    & "..\scripts\deps\$name" -OutputPath 'bin'
-    if ( -Not $? ) {
-        Pop-Location
-        exit $lastExitCode
-    }
-}
-
 Pop-Location
 exit 0
