8269342608ec3de9269eff3eadb3abb65f3ed590
Connection Refused <cutecr@pm.me>
2021-11-07 23:13:35 +0800

    [Other] Update dependencies

--- Other/geoip/build.ps1
+++ Other/geoip/build.ps1
@@ -2,7 +2,7 @@ Set-Location (Split-Path $MyInvocation.MyCommand.Path -Parent)
 
 try {
     Invoke-WebRequest `
-        -Uri 'https://github.com/v2fly/geoip/releases/download/202110140026/geoip.dat' `
+        -Uri 'https://github.com/v2fly/geoip/releases/latest/download/geoip.dat' `
         -OutFile 'geoip.dat'
 }
 catch {
