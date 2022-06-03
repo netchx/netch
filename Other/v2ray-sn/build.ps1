dcf83c01130a83960e07eeeba2cc6c793929962d
H1JK <hell0jack@protonmail.com>
2022-06-03 03:36:29 +0000

    fix build

--- Other/v2ray-sn/build.ps1
+++ Other/v2ray-sn/build.ps1
@@ -12,11 +12,11 @@ Invoke-WebRequest -Uri 'https://gist.githubusercontent.com/H1JK/b3165a99b635dcc0
 # Download Simple-Obfs plugin
 Invoke-WebRequest -Uri 'https://gist.githubusercontent.com/H1JK/b3165a99b635dcc06101690e4c43b5fd/raw/691b471f3b395a949d03a3d064d93d319d4997b7/obfs.go' -OutFile '.\proxy\shadowsocks\plugin\self\obfs.go'
 
-# Enable ReadV (Use v2fly/v2ray-core's ReadV code)
+# Enable ReadV (Use old ReadV code)
 Remove-Item '.\common\buf\io.go'
 Remove-Item '.\common\buf\readv_reader.go'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/v2fly/v2ray-core/master/common/buf/io.go' -OutFile '.\common\buf\io.go'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/v2fly/v2ray-core/master/common/buf/readv_reader.go' -OutFile '.\common\buf\readv_reader.go'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SagerNet/v2ray-core/2711fd1/common/buf/io.go' -OutFile '.\common\buf\io.go'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SagerNet/v2ray-core/2711fd1/common/buf/readv_reader.go' -OutFile '.\common\buf\readv_reader.go'
 
 $Env:CGO_ENABLED='0'
 $Env:GOROOT_FINAL='/usr'
