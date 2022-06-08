dcae72f4d3359918da6aca23a9d1f5e27838d381
Hellojack <106379370+H1JK@users.noreply.github.com>
2022-06-08 15:44:43 +0800

    Bump version to 1.9.6-Beta1

--- Netch/Controllers/UpdateChecker.cs
+++ Netch/Controllers/UpdateChecker.cs
@@ -15,7 +15,7 @@ public static class UpdateChecker
     public const string Name = @"Netch";
     public const string Copyright = @"Copyright © 2019 - 2022";
 
-    public const string AssemblyVersion = @"1.9.5";
+    public const string AssemblyVersion = @"1.9.6-Beta1";
     private const string Suffix = @"";
 
     public static readonly string Version = $"{AssemblyVersion}{(string.IsNullOrEmpty(Suffix) ? "" : $"-{Suffix}")}";
